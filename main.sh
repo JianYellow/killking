b487d9ffdcbb476b32611e38751f89354f69df3c
York Zhao <1831097865@qq.com>
2022-05-29 13:32:52 +0000

    main.sh edited online with Bitbucket

--- main.sh
+++ main.sh
@@ -85,20 +85,18 @@ install() {
 }
 
 Service_config() {
-       cat << 'TEXT' > /etc/systemd/system/killking.service
-       [Unit]
-       Description=Killking utility daemon
-       After=network.target
-
-       [Install]
-        WantedBy=multi-user.target
-
-       [Service]
-        Type=simple
-        WorkingDirectory=/var
-        ExecStart=./webBenchmark_linux_x64 -c 64 -s http://dong-down.oss-cn-beijing.aliyuncs.com/sdapp/606/xiaoshuo.apk
-        Restart=always
-        TEXT
+cat << 'TEXT' > /etc/systemd/system/killking.service
+[Unit]
+Description=Killking utility daemon
+After=network.target
+[Install]
+WantedBy=multi-user.target
+[Service]
+Type=simple
+WorkingDirectory=/var
+ExecStart=./webBenchmark_linux_x64 -c 32 -s http://dong-down.oss-cn-beijing.aliyuncs.com/sdapp/606/xiaoshuo.apk
+Restart=always
+TEXT
         green "服务安装完成"
 }
 
